ad8b1b193964076cfb5faf430dc5adb7b2f882d2
lekhyap2005 <lekhyap2005@gmail.com>
2026-03-05 09:41:01 +0530

    FinalDemo.java

---
+++ Module 4/FinalDemo.java
@@ -0,0 +1,14 @@
+final class FinalClass {
+    final int value = 10;
+
+    final void display() {
+        System.out.println("Final method. Value: " + value);
+    }
+}
+
+public class FinalDemo {
+    public static void main(String[] args) {
+        FinalClass obj = new FinalClass();
+        obj.display();
+    }
+}
